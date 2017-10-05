5c5216b67d82793dd087e3594fce1e2db384beee
Karsten Hopp <karsten@redhat.com>
2017-10-05 17:33:09 +0200

    add script to do the README.md changes

---
+++ update-readme.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# updates the README.md with the latest successful builds
+# New builds will NOT be added automatically to that file !
+
+built_modules=`mktemp`
+
+echo "Getting list of module builds, this will take a while"
+mbs-build overview --finished --limit 800 | grep " ready " |  tee $built_modules
+modules=$(grep -e "^| " README.md | cut -d '|' -f 2| grep "\`" | sed -e "s/\`//g;s/ //")
+
+for module in $modules; do
+  latest_build=`grep -m 1 $module $built_modules | sed -e "s/ */ /"`
+  if [ "x$latest_build" == "x" ]; then
+     continue
+  fi
+  module_line=$(grep "| \`$module\`" README.md)
+  field1=""
+  field2=""
+  field3=""
+  field4=""
+  field5=""
+  field6=""
+  field7=""
+  field1="$(echo $module_line | cut -d '|' -f 2 | sed -e 's/[ ]+//g')"
+  field2="$(echo $module_line | cut -d '|' -f 3 | sed -e 's/[ ]+//g')"
+  field3="$(echo $module_line | cut -d '|' -f 4 | sed -e 's/[ ]+//g')"
+  field4="$(echo $module_line | cut -d '|' -f 5 | sed -e 's/[ ]+//g')"
+  field5="$(echo $module_line | cut -d '|' -f 6 | sed -e 's/[ ]+//g')"
+  field6="$(echo $module_line | cut -d '|' -f 7 | sed -e 's/[ ]+//g')"
+  field7="$(echo $module_line | cut -d '|' -f 8 | sed -e 's/[ ]+//g')"
+  build_id=`echo $latest_build | cut -d ' ' -f 1`
+  build_date=`echo $latest_build | cut -d ' ' -f 3 | sed -e "s/T.*//"`
+  if [ "x$build_id" != "x" -a "x$build_date" != "x" ]; then
+     field7="[**$build_date**](https://mbs.fedoraproject.org/module-build-service/1/module-builds/$build_id)"
+#        [**2017-10-03**](https://mbs.fedoraproject.org/module-build-service/1/module-builds/1037)
+  fi
+  module_str="| $field1 | $field2 | $field3 | $field4 | $field5 | $field6 | $field7 |"
+  sed -i -e "s@^| \`$module\`.*@$module_str@" README.md
+done
+rm -f $built_modules
